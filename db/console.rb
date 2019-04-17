0cad82d08445f1f623237740610fc57133eab8a3
Mateusz <mateusz.trajdos@gmail.com>
2019-04-17 16:12:15 +0100

    done

--- db/console.rb
+++ db/console.rb
@@ -11,7 +11,8 @@ artist1.save()
 album1 = Album.new({'title' => 'Drones', 'genre' => 'rock', 'artist_id' => artist1.id})
 album1.save
 
-
-
-
+albums = Album.all()
+album1.genre = "Hard Rock"
+album1.update
+binding.pry
 nil
--- models/album.rb
+++ models/album.rb
@@ -24,6 +24,13 @@ attr_reader :id, :artist_id
 
     end
 
+    def update()
+      sql = "UPDATE albums SET (title, genre, artist_id) = ($1, $2, $3) WHERE id = $4;"
+      values = [@title, @genre, @artist_id, @id]
+      SqlRunner.run(sql, values)
+
+    end
+
 
     def artist()
       sql ="SELECT * FROM artists WHERE id = $1;"
